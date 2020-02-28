d8ce228f3830726c5a0d4815e40c6b534811bf0d
Cornel Pv <cornel.pacalev@gmail.com>
2020-02-28 22:47:54 +0200

    create sos blinking

---
+++ arduino-learning/custom/1_sos/1_sos.ino
@@ -0,0 +1,46 @@
+/*
+blink the SOS code: 3 long, 3 short, 3 long, pause, repeat
+*/
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  blinkLong();
+  blinkShort();
+  blinkLong();
+  delay(2000);
+}
+
+void blinkLong(){
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(800);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(800);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(800);
+}
+
+void blinkShort(){
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+  delay(800);
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+  delay(800);
+  digitalWrite(13, HIGH);
+  delay(200);
+  digitalWrite(13, LOW);
+  delay(800);
+}
